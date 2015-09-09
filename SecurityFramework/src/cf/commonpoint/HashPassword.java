d847b56758711a0a634b458a31bf458a0b24889b
SanjeetSR <sisk1994@yahoo.com>
2015-09-09 20:39:42 +0530

    Setup

---
+++ SecurityFramework/src/cf/commonpoint/HashPassword.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cf.commonpoint;
+
+/**
+ *
+ * @author Sanjeet Singh R
+ */
+public class HashPassword {
+    
+    private 
+    public static HashPassword getHash(String Password)
+    {
+        
+    }
+    
+}
