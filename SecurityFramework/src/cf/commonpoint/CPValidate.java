c6e18b84793543df3a24b160c8adca0e28222f4a
SanjeetSR <sisk1994@yahoo.com>
2015-09-20 05:00:38 +0530

    CPValidate.java

---
+++ SecurityFramework/src/cf/commonpoint/CPValidate.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cf.commonpoint;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Sanjeet Singh R
+ */
+public class CPValidate {
+    public static String CPValidate(String rawText, String type)
+    {
+        int statusCode;
+        /* 0-Bad Input
+           1-Valid
+           2-Invalid type */ 
+        Matcher matcher;
+        Pattern pattern = null;
+        switch(type)
+        {
+            case "username": pattern=Pattern.compile("^[a-zA-Z0-9-_]{6,15}$");break;
+            case "person-name": pattern=Pattern.compile("^[a-zA-Z]+(([',.- ][a-zA-Z ])?[a-zA-Z]*)*$");break;
+            case "email" : pattern=Pattern.compile("^[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@"
+                +"[a-zA-Z0-9-]+(\\.[a-zA-Z0-9]+)*(\\.[a-zA-z]{2,})$");break;
+            case "password": pattern=Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A_Z]).{4,8}$");break;
+            case "phone-number":pattern=Pattern.compile("^[0-9]{10}$");break;
+            default: break;
+        }
+        if(pattern!=null)
+        {
+            matcher=pattern.matcher(rawText);
+            if(matcher.matches())
+                statusCode=1;
+            else statusCode=0;
+        }
+        else statusCode=2;
+        return rawText;
+    }
+    
+}
