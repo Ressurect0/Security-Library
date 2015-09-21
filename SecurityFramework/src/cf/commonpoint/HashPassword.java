9d6038480f6339d0c10d4598fa9882f02807dad7
SanjeetSR <sisk1994@yahoo.com>
2015-09-21 11:41:43 +0530

    Password Hashing

--- SecurityFramework/src/cf/commonpoint/HashPassword.java
+++ SecurityFramework/src/cf/commonpoint/HashPassword.java
@@ -5,16 +5,33 @@
  */
 package cf.commonpoint;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
 /**
  *
  * @author Sanjeet Singh R
  */
 public class HashPassword {
     
-    private 
-    public static HashPassword getHash(String Password)
+    final static int SALT_SIZE=64;
+    final static int ITERATION_COUNT=10000;
+    final static int KEYLENGTH=64;
+    
+    public static byte[] getHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException
     {
+        byte[] hash=null;
+        byte[] salt = new byte[SALT_SIZE];
+        SecureRandom random=new SecureRandom();
+        random.nextBytes(salt);
         
+        SecretKeyFactory secretKeyFactory=SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec pbeKeySpec=new PBEKeySpec(password.toCharArray(), salt, ITERATION_COUNT, KEYLENGTH);
+        hash=secretKeyFactory.generateSecret(pbeKeySpec).getEncoded();
+        return hash;        
     }
     
 }
