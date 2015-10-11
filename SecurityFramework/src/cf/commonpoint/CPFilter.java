815fc7932176eb3846f2f8a7676fb3d5e2c60b59
SanjeetSR <sisk1994@yahoo.com>
2015-10-11 15:57:58 +0530

    Filter Security

--- SecurityFramework/src/cf/commonpoint/CPFilter.java
+++ SecurityFramework/src/cf/commonpoint/CPFilter.java
@@ -9,16 +9,31 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cf.commonpoint.cputility.XMLParser;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Sanjeet Singh R
  */
 public class CPFilter {
-    private static void redirect(HttpServletRequest request,HttpServletResponse response) throws IOException
+    private static void redirect(String indicator,HttpServletRequest request,HttpServletResponse response) throws IOException, ParserConfigurationException, SAXException
     {
-        String serverName=request.getServerName();
-        String requestURI=request.getRequestURI();
-        response.sendRedirect("https://"+serverName+requestURI);
+        switch(indicator)
+        {
+            case "https":   
+                            String serverName=request.getServerName();
+                            String requestURI=request.getRequestURI();
+                            response.sendRedirect("https://"+serverName+requestURI);
+            case "login":
+                            String uri="C:\\Security-Framework\\SecurityFramework\\src\\cf\\commonpoint\\config\\cpconfig.xml";
+                            String node="login-page";
+                            String redirectURI=XMLParser.getNodeValue(uri, node);
+                            response.sendRedirect(redirectURI);
+                
+        }
+        
     }
     
 }
